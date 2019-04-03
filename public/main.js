f742f9d246ea40780af823eb815381ff99e5e8f4
Brandt Hill <brandtlarsonhill@gmail.com>
2019-04-02 23:58:26 -0500

    Added some js to do some ajax stuff for contact form

--- public/main.js
+++ public/main.js
@@ -1,3 +1,25 @@
-function thing() {
-    alert('Hello World!')
-}
+window.addEventListener("load", () => {
+
+    let sendData = () => {
+        var XHR = new XMLHttpRequest()
+
+        XHR.addEventListener("load", (event) => {
+            alert(event.target.responseText)
+        })
+
+        XHR.addEventListener("error", (event) => {
+            alert("Something went awry!")
+        })
+
+        XHR.open("POST", "http://localhost/contact")
+
+        XHR.send(new FormData(form))
+    }
+
+    var form = document.getElementById("contactForm")
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault()
+        sendData()
+    })
+})
